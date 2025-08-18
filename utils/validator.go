ebe460893f81c666e791a41cf24cdf9b59c6786b
OriginalAuthority <luke@telepedia.net>
2025-08-18 16:51:13 +0100

    Add Util for validating a thumb request is valid

--- utils/validator.go
+++ utils/validator.go
@@ -42,3 +42,34 @@ func ValidateImageRequest(req models.ImageRequest) error {
 
 	return nil
 }
+
+func ValidateThumbnailRequest(req models.ThumbnailRequest) error {
+	if req.Wiki == "" {
+		return ErrInvalidWiki
+	}
+
+	if req.Hash1 == "" || len(req.Hash1) > 2 {
+		return ErrInvalidHash
+	}
+
+	if req.Hash2 == "" || len(req.Hash2) > 3 || len(req.Hash2) < 2 || !strings.HasPrefix(req.Hash2, req.Hash1) {
+		return ErrInvalidHash
+	}
+
+	if req.Filename == "" {
+		return ErrInvalidFileName
+	}
+
+	if req.Revision == "" {
+		return ErrInvalidRevision
+	}
+
+	if req.Width == "" {
+		return ErrInvalidWidth
+	}
+
+	// we need to check that the revision is valid here in MediaWiki format
+	// but I can't deal with doing that rn so maybe later
+
+	return nil
+}
