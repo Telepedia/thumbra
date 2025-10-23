530b67968d379515762904cdbc206cf7cda7212b
OriginalAuthority <luke@telepedia.net>
2025-10-23 13:30:54 +0100

    Add Access-Control-Allow-Origin header

--- middleware/headers.go
+++ middleware/headers.go
@@ -7,6 +7,7 @@ import (
 func ImageResponseMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Backend", "Thumbra")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		next.ServeHTTP(w, r)
 	})
